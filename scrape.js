b3addf545f2fcb584880f91c593d4ca0555a9191
Ahnaf Shahriar <shahriarahnaf007@gmail.com>
2021-06-24 23:59:23 -0400

    Counter loop started take a look tmmrw

--- scrape.js
+++ scrape.js
@@ -21,32 +21,34 @@ request('https://www.espn.com/soccer/fixtures/_/league/uefa.euro', (error, respo
             counterPerDay.push(0); //increase length of array to hold extra table of matches
         
      
-        
-            $('tr').each((i, element) => {
-                //get status of match
-                const status = $(element)
-                .find('.record')
+            $('.responsive-table-wrap').each((x, ele) => {
+                    
+                $('tr').each((i, element) => {
+                    //get status of match
+                    const status = $(element)
+                    .find('.record')
+                    .text();
+
+                    //get left side team
+                    const team1 = $(element)
+                    .find('td')
+                    .find('span:first')
+                    .text()
+
+                //get right side team
+                const team2 = $(element)
+                .find('td')
+                .find('span:last')
                 .text();
 
-                //get left side team
-                const team1 = $(element)
-                .find('td')
-                .find('span:first')
-                .text()
-
-            //get right side team
-            const team2 = $(element)
-            .find('td')
-            .find('span:last')
-            .text();
-
-            //push into array for later
-            statusArr.push(status);
-            teamSideA.push(team1);
-            teamSideB.push(team2);
-             //increase counter number in hte array
-              counterPerDay[n]= i;
-            });
+                //push into array for later
+                statusArr.push(status);
+                teamSideA.push(team1);
+                teamSideB.push(team2);
+                //increase counter number in hte array
+                counterPerDay[n]= i;
+                });
+             });
         });
     }
 
